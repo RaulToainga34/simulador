c997b3dd95a410eed492d6dec4dbc763be6ed948
RaulToainga34 <rabetomo542@gmail.com>
2026-04-24 21:07:03 -0500

    Funcion calcular

--- simulador.js
+++ simulador.js
@@ -1,1 +1,15 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+
+function calcular(){
+    let vIngresos = document.getElementById("txtIngresos");
+    let ingresosStr = vIngresos.value;
+    let ingresosBase = parseFloat(ingresosStr);
+
+    let vEgresos = document.getElementById("txtEgresos");
+    let egresosStr = vEgresos.value;
+    let egresosBase = parseFloat(egresosStr);
+
+    let resultado = calcularDisponible(ingresosBase, egresosBase);
+    let mostrarResultado = document.getElementById("spnDisponible");
+    mostrarResultado.textContent = resultado;
+}
