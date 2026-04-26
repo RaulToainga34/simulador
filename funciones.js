73418015c9cd23bde471c22d5f713a5ec60fc850
RaulToainga34 <rabetomo542@gmail.com>
2026-04-25 22:01:33 -0500

    Se realizo el cambio requerido

--- funciones.js
+++ funciones.js
@@ -1,6 +1,7 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
 
-function calcularDisponible(ingresos,egresos){
+function calcularDisponible(ingresos,vivienda, alimentacion,varios){
+    let egresos = vivienda+alimentacion+varios;
     let disponible = ingresos - egresos;
     if (disponible < 0){
         return 0;
--- simulador.js
+++ simulador.js
@@ -1,11 +1,11 @@
-//AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
-
 function calcular(){
 
     limpiarErrores();
 
     let ingresos = document.getElementById("txtIngresos");
-    let egresos = document.getElementById("txtEgresos");
+    let egresosArriendo = document.getElementById("txtArriendo");
+    let egresosAlimentacion = document.getElementById("txtAlimentacion");
+    let egresosVarios = document.getElementById("txtVarios");
     let monto = document.getElementById("txtMonto");
     let plazo = document.getElementById("txtPlazo");
     let tasa = document.getElementById("txtTasaInteres");
@@ -17,8 +17,18 @@ function calcular(){
         valido = false;
     }
 
-    if (!egresos.checkValidity()){
-        mostrarError("errEgresos", egresos, "Ingrese un valor válido");
+    if (!egresosArriendo.checkValidity()){
+        mostrarError("errEgresos", egresosArriendo, "Ingrese arriendo válido");
+        valido = false;
+    }
+
+    if (!egresosAlimentacion.checkValidity()){
+        mostrarError("errEgresos", egresosAlimentacion, "Ingrese alimentación válida");
+        valido = false;
+    }
+
+    if (!egresosVarios.checkValidity()){
+        mostrarError("errEgresos", egresosVarios, "Ingrese varios válido");
         valido = false;
     }
 
@@ -40,12 +50,18 @@ function calcular(){
     if (!valido) return;
 
     let ingresosBase = parseFloat(ingresos.value);
-    let egresosBase = parseFloat(egresos.value);
+    let egresosBaseAR = parseFloat(egresosArriendo.value);
+    let egresosBaseAL = parseFloat(egresosAlimentacion.value);
+    let egresosBaseV = parseFloat(egresosVarios.value);
     let montoBase = parseFloat(monto.value);
     let plazoBase = parseInt(plazo.value);
     let tasaBase = parseFloat(tasa.value);
 
-    let disponible = calcularDisponible(ingresosBase, egresosBase);
+    // NUEVO: suma de gastos
+    let totalGastos = egresosBaseAR + egresosBaseAL + egresosBaseV;
+    document.getElementById("spnTotalGastos").textContent = totalGastos;
+
+    let disponible = calcularDisponible(ingresosBase, egresosBaseAR, egresosBaseAL,egresosBaseV);
     document.getElementById("spnDisponible").textContent = disponible;
 
     let capacidadPago = calcularCapacidadPago(disponible);
